a93cf28e264dca93eb125f5226f4fdbe98835f34
Bernard Huff <bernard.huff008@gmail.com>
2018-09-30 17:14:06 -0500

    Cleaned up the content of the code a little bit by deleting commented out text.

--- OneStepJournal/JournalTableViewController.swift
+++ OneStepJournal/JournalTableViewController.swift
@@ -20,17 +20,17 @@ class JournalTableViewController: UITableViewController {
         cameraButton.imageView?.contentMode = .scaleAspectFit
         plusButton.imageView?.contentMode = .scaleAspectFit
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToNew", sender: "camera")
     }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToNew", sender: nil)
+    }
+    
 
     // MARK: - Table view data source
 
