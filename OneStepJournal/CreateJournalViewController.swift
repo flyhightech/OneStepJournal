cca18ea095eef8b2563b759afd1769280d0e35c2
Bernard Huff <bernard.huff008@gmail.com>
2018-10-25 19:32:27 -0500

    Sweep

--- OneStepJournal/CreateJournalViewController.swift
+++ OneStepJournal/CreateJournalViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var journalTextView: UITextView!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var setButton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cancelBtnTapped(_ sender: Any) {
+        
+        
     }
     
-
-
+    @IBAction func saveBtnTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func setDateTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func calenderBtnTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func cameraBtnTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 } //End of the code on this view controller
