3afb60dafea3ecb4d6fa8b6bf81d23b619902da8
Bernard Huff <bernard.huff008@gmail.com>
2019-03-25 02:01:48 -0500

    Sweep

--- OneStepJournal/JournalDetailViewController.swift
+++ OneStepJournal/JournalDetailViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class JournalDetailViewController: UIViewController {
+    
+    @IBOutlet weak var journalTextLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    var entry : Entry?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +21,6 @@ class JournalDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
